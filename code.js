088643f1c5f349fea39270065a3ec8856fdb63ee
Tinedanzer <johnhillallen@gmail.com>
2018-08-04 22:35:59 -0500

    more

--- code.js
+++ code.js
@@ -1,5 +1,33 @@
-// insert firebase here
+var config = {
+    apiKey: "AIzaSyDU-Ve4ffAdpzGw3tLOvIVLGNRimKKwuPU",
+    authDomain: "aroo-8ee3a.firebaseapp.com",
+    databaseURL: "https://aroo-8ee3a.firebaseio.com",
+    projectId: "aroo-8ee3a",
+    storageBucket: "aroo-8ee3a.appspot.com",
+    messagingSenderId: "360578690037",
+  };
+firebase.initializeApp(config);
+var names="";
+var destinations="";
+var minutes= 0;
+var frequencys= 0;
 
-// target form data, have it store it in firebase
+$("#addTrain").on("click",function(){
+  names = $("#train1").val().trim();
+  destinations = $("#destination1").val().trim();
+  minutes = $("#time1").val().trim();
+  frequencys = $("#frequency1").val().trim();
 
+  firebase.database().ref().set({
+    names:name.name1,
+    destinations:destination.destination1,
+    minutes:minute.minute1,
+    frequencys:frequency.frequency1,
+
+  })
+});
 // from firebase append data for each slot on the html
+// target form data, have it store it in firebase, have the timers run in real time; video 1
+firebase.database().ref().on("value",function(snap){
+  $('#minutes').html(snap.val().minute.minute1);
+});
