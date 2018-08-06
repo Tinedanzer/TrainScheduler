46edbd4dd7ade65d420994a2f7bb797ee8eee816
Tinedanzer <johnhillallen@gmail.com>
2018-08-06 14:00:07 -0500

    clean up for inspection

--- code.js
+++ code.js
@@ -1,36 +1,40 @@
 var config = {
-    apiKey: "AIzaSyDU-Ve4ffAdpzGw3tLOvIVLGNRimKKwuPU",
-    authDomain: "aroo-8ee3a.firebaseapp.com",
-    databaseURL: "https://aroo-8ee3a.firebaseio.com",
-    projectId: "aroo-8ee3a",
-    storageBucket: "aroo-8ee3a.appspot.com",
-    messagingSenderId: "360578690037",
-  };
+  apiKey: "AIzaSyDU-Ve4ffAdpzGw3tLOvIVLGNRimKKwuPU",
+  authDomain: "aroo-8ee3a.firebaseapp.com",
+  databaseURL: "https://aroo-8ee3a.firebaseio.com",
+  projectId: "aroo-8ee3a",
+  storageBucket: "aroo-8ee3a.appspot.com",
+  messagingSenderId: "360578690037"
+};
 firebase.initializeApp(config);
+
 var trains = "";
 var destinations = "";
-var minutes2 = 0;
+var minutes = 0;
 var frequencys = 0;
 console.log(trains)
 
 $("#addTrain").on("click",function(){
+  event.preventDefault();
   
   trains = $("#train1").val().trim();
   destinations = $("#destination1").val().trim();
-  minutes2 = $("#time1").val().trim();
+  minutes = $("#time1").val().trim();
   frequencys = $("#frequency1").val().trim();
   console.log(trains);console.log(destinations);
-  firebase.database().ref().set({
+  firebase.database().ref().push({
     trains:trains,
     destinations:destinations,
-    minutes2:minutes2,
-    frequencys:frequencys
-
+    minutes:minutes,
+    frequencys:frequencys,
+    dateadded:firebase.database.Servervalue.TIMESTAMP
   });
   console.log(trains);console.log(destinations);
 });
-// from firebase append data for each slot on the html
-// target form data, have it store it in firebase, have the timers run in real time; video 1
+
+// for Antonio to test if data has been sent under MINUTES in the database
+// I have preprogrammed 15 minutes into the database under "minutes"
+// push should be changing it
 firebase.database().ref().on("value",function(snap){
   $('#minutes').html(snap.val().minutes);
 });
