0c39aa18f00e2a44298f3ef990d64f12f6e2e74a
Tinedanzer <johnhillallen@gmail.com>
2018-08-07 13:57:34 -0500

    database function; last step is to add moment again

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+// Makes a connection to firebase.
 var config = {
   apiKey: "AIzaSyDU-Ve4ffAdpzGw3tLOvIVLGNRimKKwuPU",
   authDomain: "aroo-8ee3a.firebaseapp.com",
@@ -7,34 +9,46 @@ var config = {
   messagingSenderId: "360578690037"
 };
 firebase.initializeApp(config);
-
-var trains = "";
-var destinations = "";
-var minutes = 0;
-var frequencys = 0;
-console.log(trains)
+// establishing variables that will be fill from form data
+  var trains = "";
+  var destinations = "";
+  var minutes = 0;
+  var frequencys = 0;
 
 $("#addTrain").on("click",function(){
   event.preventDefault();
-  
+
   trains = $("#train1").val().trim();
   destinations = $("#destination1").val().trim();
   minutes = $("#time1").val().trim();
   frequencys = $("#frequency1").val().trim();
-  console.log(trains);console.log(destinations);
+
   firebase.database().ref().push({
     trains:trains,
     destinations:destinations,
     minutes:minutes,
     frequencys:frequencys,
-    dateadded:firebase.database.Servervalue.TIMESTAMP
+    dateAdded:firebase.database.ServerValue.TIMESTAMP
   });
+  $("#Aroo2")[0].reset();
+  
   console.log(trains);console.log(destinations);
 });
 
-// for Antonio to test if data has been sent under MINUTES in the database
-// I have preprogrammed 15 minutes into the database under "minutes"
-// push should be changing it
-firebase.database().ref().on("value",function(snap){
-  $('#minutes').html(snap.val().minutes);
+firebase.database().ref().on("child_added", function(snap){
+  
+  $('#train').append("<tr><td>" + snap.val().trains + "</td></tr>");
+  $('#destination').append("<tr><td>"+snap.val().destinations+"</td></tr>");
+  $('#frequency').append("<tr><td>"+snap.val().frequencys+"</td></tr>");
+  $('#minutes').append("<tr><td>"+snap.val().minutes+"</td></tr>");
+})
+
 });
+// this code isnt needed, but will keep for future reference.
+
+// firebase.database().ref().orderByChild("dateAdded").limitToLast(1).on("child_added",function(snap){  
+//   $('#train').html(snap.val().trains);
+//   $('#destination').html(snap.val().destinations);
+//   $('#frequency').html(snap.val().frequencys);
+//   $('#minutes').html(snap.val().minutes);
+// });
